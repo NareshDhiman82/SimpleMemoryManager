f82fd48d2f8dc51c26fbf8df2622937a4008601b
wings <desireforwing@gmail.com>
2020-10-02 14:21:24 +0530

    No change in initial design idea. Updated for garbage collection. Updated to add comments. updated to correct typos, and build errors.

--- SimpleMemoryManager/SimpleMemoryManager.cpp
+++ SimpleMemoryManager/SimpleMemoryManager.cpp
@@ -1,73 +1,76 @@
-#define MaxRequest 64
+#include <stdio.h> 
+
+#define MaxRequest 8
 #define MaxMemory  1024
 
 void* MyMalloc(int Size);
 void  Myfree(void*);
-
 char MemBuff[MaxMemory];
 
 typedef struct _FreeList {
-	void* Start
-		int   Size;
+	void* Start;  //Corrected typo, missing ';'
+	int   Size;
 	bool  InUse;
 }FreeListTyp;
 
 typedef struct _Metadata {
-	FreeListTyp FreeList[MaxRequest];
+	FreeListTyp FreeList[MaxRequest + 1]; //Need one extra slot.
 }MetadataTyp;
 
-MetadataTyp* Metadata = MemBuff;
-void* BuffStart = Metadata + sizeof(Metadata);
+MetadataTyp* Metadata = (MetadataTyp*)MemBuff; //type casting was missing.
+void* BuffStart = (char*)Metadata + sizeof(MetadataTyp);
 
-void StartUp();
+void StartUp()
 {
 	//initialize all slots with default.
-	for (i = 1; i < MaxRequest; i++)
+	for (int i = 1; i < MaxRequest; i++) //int missing for i
 	{
-		Metadata->FreeList[i].InUse = FALSE;
+		Metadata->FreeList[i].InUse = false;
 		Metadata->FreeList[i].Size = 0;
 		Metadata->FreeList[i].Start = NULL;
 	}
 
-	//initialize first Slot with full avalible chunk 
+	//Iinitialize first Slot with full available chunk 
 	Metadata->FreeList[0].Size = MaxMemory - sizeof(Metadata);
 	Metadata->FreeList[0].Start = BuffStart;
-	Metadata->FreeList[0].InUse = FALSE;
-
+	Metadata->FreeList[0].InUse = false;
 }
 void* FindChunk(int Size, int* Slot)
 {
 	int i = 0;
 	void* RetPtr = NULL;
 	for (i = 0; i < MaxRequest; i++)
-		if ((Metadata->FreeList[i].Size <= Size) && (Metadata->FreeList[i].InUse == FALSE))
+		if ((Metadata->FreeList[i].Size >= Size) && (Metadata->FreeList[i].InUse == false)) //corrected typo in operator
 		{
-			Metadata->FreeList[i].InUse = TRUE;
-			Metadata->FreeList[i].Size -= Size;
+			//Modify next Slot info
+			Metadata->FreeList[i + 1].InUse = false;
+			Metadata->FreeList[i + 1].Size = Metadata->FreeList[i].Size - Size;
+			Metadata->FreeList[i + 1].Start = ((char*)(Metadata->FreeList[i].Start)) + Size;
+			Metadata->FreeList[i].InUse = true;
+			Metadata->FreeList[i].Size = Size;
 			RetPtr = Metadata->FreeList[i].Start;
-			Metadata->FreeList[i].Start += Size;
+			//Metadata->FreeList[i].Start += Size; // Commented, This was left in error (Typo)
 			*Slot = i;
+			break;
 		}
 	return RetPtr;
 }
 
 void findAndFreeChunk(int Slot)
 {
-
-	Metadata->FreeList[Slot].InUse = FALSE;
-
-	//Check if next availble Slot is not inuse if so then merge
-	if (Metadata->FreeList[Slot + 1].InUse == FASLE)
+	//Mark the slot as free, and perfrom garbage collection.
+	Metadata->FreeList[Slot].InUse = false;
+	//Mergs consicutive smaller memoery block to single bigger memory block
+    //going reverese, Starting from one slot next form the curretn.
+	for (int i = (Slot + 1); i > 0; i--)
 	{
-		Metadata->FreeList[Slot].Size = Metadata->FreeList[Slot + 1].Size
+		if ((Metadata->FreeList[i].InUse == false) &&
+			(Metadata->FreeList[i - 1].InUse == false))
+		{
+			Metadata->FreeList[i - 1].Size += Metadata->FreeList[i].Size;
+			Metadata->FreeList[i].Size = 0;
+		}
 	}
-
-	//Check if previous availble Slot is not inuse if so then merge
-	if (Metadata->FreeList[Slot - 1].InUse == FASLE)
-	{
-		Metadata->FreeList[Slot - 1].Size = Metadata->FreeList[Slot].Size
-	}
-
 }
 
 void* MyMalloc(int Size)
@@ -75,25 +78,26 @@ void* MyMalloc(int Size)
 	void* Chunk;
 	int    Slot;
 
-	//Find bing enough Chunk
-	Chunk = FindChunk(Size + sizeof(slot), &Slot
-}
-if (!Chunk)
-return NULL; //Error
-else
-{
-	*((int*)Chunk) = Slot;
-	((int*)Chunk)++;
-}
-return Chunk;
+	//Find big enough Chunk
+	Chunk = FindChunk(Size + sizeof(Slot), &Slot); //Updated to correct typo
+
+	if (!Chunk)
+		return NULL; //Error
+	else
+	{
+		//Add Slot index, this will help in quick free operation.
+		*((int*)Chunk) = Slot;
+	}
+	return ((int*)Chunk) + 1; //Updated
 }
 
 void Myfree(void* AddrIn)
 {
 
 	int* SlotVal = (int*)AddrIn;
+	if (SlotVal == NULL) return;  //Updated: Handled the case of NULL pointer 
 	SlotVal--;
 
 	findAndFreeChunk(*SlotVal);
-
 }
+
--- SimpleMemoryManager/TestDriver.cpp
+++ SimpleMemoryManager/TestDriver.cpp
@@ -3,9 +3,166 @@
 
 #include <iostream>
 
-int main()
+extern void StartUp();
+extern void* MyMalloc(int SizeIn);
+extern void  Myfree(void* AddrIn);
+
+#define MAXRETVAL 10
+void* Ret[MAXRETVAL];
+
+void PrepareTest()
 {
-    std::cout << "Hello World!\n";
+	for (int i = 0; i < MAXRETVAL ; i++)
+		Ret[i] = NULL;
+}
+
+void DiaplayRetVal()
+{
+	for (int i = 0; i < MAXRETVAL; i++)
+		printf("Ret[%d] = %#x\n", i, (int)Ret[i]);
+}
+
+int main(void)
+{
+	
+
+	//Setup the Mem Manager
+	StartUp();
+
+	//Test1:  1 Allocation and 1 Free ; Free is in order.
+	PrepareTest();
+	Ret[0] = MyMalloc(2);
+	Myfree(Ret[0]);
+	DiaplayRetVal();
+
+	//Test2:  2 Allocation and 2 Free ; Free is in order.
+	PrepareTest(); 
+	Ret[0] = MyMalloc(4);
+	Ret[1] = MyMalloc(2);
+	Myfree(Ret[0]);
+	Myfree(Ret[1]);
+	DiaplayRetVal();
+
+	//Test3:  4 Allocation and 4 Free ; Free is in order.
+	PrepareTest(); 
+	Ret[0] = MyMalloc(4);
+	Ret[1] = MyMalloc(8);
+	Ret[2] = MyMalloc(4);
+	Ret[3] = MyMalloc(8);
+	Myfree(Ret[0]);
+	Myfree(Ret[1]);
+	Myfree(Ret[2]);
+	Myfree(Ret[3]);
+	DiaplayRetVal();
+
+	//Test4:  8 Allocation and 8 Free ; Free is in order.
+	PrepareTest(); 
+	Ret[0] = MyMalloc(4);
+	Ret[1] = MyMalloc(8);
+	Ret[2] = MyMalloc(4);
+	Ret[3] = MyMalloc(8);
+	Ret[4] = MyMalloc(4);
+	Ret[5] = MyMalloc(8);
+	Ret[6] = MyMalloc(4);
+	Ret[7] = MyMalloc(8);
+	Myfree(Ret[0]);
+	Myfree(Ret[1]);
+	Myfree(Ret[2]);
+	Myfree(Ret[3]);
+	Myfree(Ret[4]);
+	Myfree(Ret[5]);
+	Myfree(Ret[6]);
+	Myfree(Ret[7]);
+	DiaplayRetVal();
+
+
+	//Test4:  10 Allocation and 10 Free ; Free is in order.
+	//Expected: 9th, and 10th Allocation return NULL (Failed)
+	PrepareTest(); 
+	Ret[0] = MyMalloc(4);
+	Ret[1] = MyMalloc(8);
+	Ret[2] = MyMalloc(4);
+	Ret[3] = MyMalloc(8);
+	Ret[4] = MyMalloc(4);
+	Ret[5] = MyMalloc(8);
+	Ret[6] = MyMalloc(4);
+	Ret[7] = MyMalloc(8);
+	Ret[8] = MyMalloc(8);
+	Ret[9] = MyMalloc(8);
+	Myfree(Ret[0]);
+	Myfree(Ret[1]);
+	Myfree(Ret[2]);
+	Myfree(Ret[3]);
+	Myfree(Ret[4]);
+	Myfree(Ret[5]);
+	Myfree(Ret[6]);
+	Myfree(Ret[7]);
+	Myfree(Ret[8]);
+	Myfree(Ret[9]);
+	DiaplayRetVal();
+
+	//Test5:  8 Allocation and 8 Free ; Free is in Reverse order.
+	PrepareTest();
+	Ret[0] = MyMalloc(4);
+	Ret[1] = MyMalloc(8);
+	Ret[2] = MyMalloc(4);
+	Ret[3] = MyMalloc(8);
+	Ret[4] = MyMalloc(4);
+	Ret[5] = MyMalloc(8);
+	Ret[6] = MyMalloc(4);
+	Ret[7] = MyMalloc(8);
+	Myfree(Ret[7]);
+	Myfree(Ret[6]);
+	Myfree(Ret[5]);
+	Myfree(Ret[4]);
+	Myfree(Ret[3]);
+	Myfree(Ret[2]);
+	Myfree(Ret[1]);
+	Myfree(Ret[0]);
+	DiaplayRetVal();
+
+	//Test6:  8 Allocation and 8 Free ; Free is out of order.
+	PrepareTest();
+	Ret[0] = MyMalloc(4);
+	Ret[1] = MyMalloc(8);
+	Ret[2] = MyMalloc(4);
+	Ret[3] = MyMalloc(8);
+	Ret[4] = MyMalloc(4);
+	Ret[5] = MyMalloc(8);
+	Ret[6] = MyMalloc(4);
+	Ret[7] = MyMalloc(10);
+	Myfree(Ret[7]);
+	Myfree(Ret[6]);
+	Myfree(Ret[5]);
+	Myfree(Ret[2]);
+	Myfree(Ret[1]);
+	Myfree(Ret[0]);
+	Myfree(Ret[3]);
+	Myfree(Ret[4]);
+	DiaplayRetVal();
+
+	//Test7:  8 Allocation and 8 Free ; Last request consumes all remaing memory to cover corner case
+	//        Free is out of order.
+	PrepareTest();
+	Ret[0] = MyMalloc(4);
+	Ret[1] = MyMalloc(8);
+	Ret[2] = MyMalloc(4);
+	Ret[3] = MyMalloc(8);
+	Ret[4] = MyMalloc(4);
+	Ret[5] = MyMalloc(8);
+	Ret[6] = MyMalloc(4);
+	Ret[7] = MyMalloc(944);
+	Myfree(Ret[7]);
+	Myfree(Ret[6]);
+	Myfree(Ret[5]);
+	Myfree(Ret[2]);
+	Myfree(Ret[1]);
+	Myfree(Ret[0]);
+	Myfree(Ret[3]);
+	Myfree(Ret[4]);
+	DiaplayRetVal();
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
